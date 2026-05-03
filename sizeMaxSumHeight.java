e134978de63ea75f3f29638db0c4364091995ae1
Rajat <rajatgupta.0104@gmail.com>
2026-05-03 16:39:53 +0530

    updated

--- sizeMaxSumHeight.java
+++ sizeMaxSumHeight.java
@@ -16,6 +16,20 @@ public class sizeMaxSumHeight {
         
     }
 
+    public static int sum(Node root) {
+        if(root == null) return 0;
+        return sum(root.left) + sum(root.right) + root.val;
+        
+    }
+
+    public static int max(Node root) {
+        if(root == null) return Integer.MIN_VALUE;
+        int a = root.val;
+        int b = max(root.left);
+        int c = max(root.right);
+        return Math.max(a, Math.max(b,c));
+    }
+
     public static void main(String args[]) {
         Node root = new Node(1);
         Node a = new Node(2);
@@ -23,7 +37,7 @@ public class sizeMaxSumHeight {
         root.left = a;
         root.right = b;
         Node c = new Node(4);
-        Node d = new Node(5);
+        Node d = new Node(50);
         a.left = c;
         a.right = d;
         Node e = new Node(6);
@@ -31,6 +45,8 @@ public class sizeMaxSumHeight {
         b.left = e;
         b.right = f;
         // preorder(root);
-        System.out.println(size(root));
+        // System.out.println(size(root));
+        System.out.println(sum(root));
+        System.out.println(max(root));
     }
 }
