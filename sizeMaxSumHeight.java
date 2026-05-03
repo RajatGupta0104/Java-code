8380c124db1b6c538a0d07250bec7fbe677b98f3
Rajat <rajatgupta.0104@gmail.com>
2026-05-03 14:48:13 +0530

    Added 2nd file

---
+++ sizeMaxSumHeight.java
@@ -0,0 +1,35 @@
+public class sizeMaxSumHeight {
+    public static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void preorder(Node root) {
+        if(root == null) return;
+        System.out.print(root.val + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        root.left = a;
+        root.right = b;
+        Node c = new Node(4);
+        Node d = new Node(5);
+        a.left = c;
+        a.right = d;
+        Node e = new Node(6);
+        Node f = new Node(7);
+        b.left = e;
+        b.right = f;
+        preorder(root);
+    }
+}
